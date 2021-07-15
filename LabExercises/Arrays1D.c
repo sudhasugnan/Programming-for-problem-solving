212212e5255ed9461203c3c8f8ff88f10259e81e
sudhasugnan <87451845+sudhasugnan@users.noreply.github.com>
2021-07-15 12:09:32 +0530

    Add files via upload

---
+++ LabExercises/Arrays1D.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int A[10],i;  
+	printf("Enter 10 numbers");
+	 for(i=0;i<10;i++)
+	   scanf("%d",&A[i]);
+	 printf("\n");                                
+	for(i=0;i<10;i++)
+	   printf("%d   ",A[i]);
+	
+	
+	return 1;
+}
